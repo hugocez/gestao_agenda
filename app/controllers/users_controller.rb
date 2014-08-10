3736fb97573b0dc77b4327b9ea3cfb6c85b8ddf9
Hugo Cezario <hugocez@yahoo.com.br>
2014-08-10 18:47:46 -0300

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Seja Bem Vindo ao Software de Gestao de Agenda!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
 end
+
+private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
