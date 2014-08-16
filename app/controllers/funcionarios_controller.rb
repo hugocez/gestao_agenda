ebf7dbac382c8255a83702cc9f586724d7209f9a
Hugo Cezario <hugocez@yahoo.com.br>
2014-08-15 23:27:45 -0300

    finalizado funcs e usuarios

--- app/controllers/funcionarios_controller.rb
+++ app/controllers/funcionarios_controller.rb
@@ -10,12 +10,25 @@ class FuncionariosController < ApplicationController
     @funcionario = Funcionario.new(funcionario_params)
     @funcionario.empresa_id = encontrar_empresa
     if @funcionario.save
-      p = "123456"
-      user = User.create(name: params[:funcionario][:nome], email:"seuemail@email.com", password: p, password_confirmation: p, funcionario_id: @funcionario.id)
+      #p = "123456"
+      #user = User.create(name: params[:funcionario][:nome], email:"seuemail@email.com", password: p, password_confirmation: p, funcionario_id: @funcionario.id)
       flash[:success] = "Funcionario Cadastrado Com Sucesso!"
-      redirect_to edit_user_path(user)
+      redirect_to funcionario_path
     else
       render 'new'
+    end
+  end
+  
+  def update
+    @funcionario = Funcionario.find(params[:id])
+    u = User.new(usuario_params)
+    u.name = params[:funcionario][:nome]
+    if @funcionario.update_attributes(funcionario_params)
+      @funcionario.user = u
+      flash[:success] = "Cadastro Atualizado!"
+      redirect_to @funcionario
+    else
+      render 'edit'
     end
   end
 
@@ -28,6 +41,7 @@ class FuncionariosController < ApplicationController
   end
 
   def edit
+    @funcionario = Funcionario.find(params[:id])
   end
   
   def index
@@ -38,7 +52,11 @@ class FuncionariosController < ApplicationController
   private
 
     def funcionario_params
-      params.require(:funcionario).permit(:nome, :cpf, :data_nasc, :email)
+      params.require(:funcionario).permit(:nome, :cpf, :data_nasc,:email,:password,:password_confirmation)
+    end
+    
+    def usuario_params
+      params.require(:user).permit(:email,:password,:password_confirmation)
     end
     
     def signed_in_user
